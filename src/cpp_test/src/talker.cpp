49d3be456cb0dcb6142c686c5309326189df65b5
Rain649 <liushijie649bit@gmail.com>
2026-02-26 08:48:10 +0800

    增加topic，本地实现自动启动

--- src/cpp_test/src/talker.cpp
+++ src/cpp_test/src/talker.cpp
@@ -1,29 +1,46 @@
 #include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
 
 class Talker : public rclcpp::Node
 {
 public:
     Talker() : Node("talker_node")
     {
+        // 创建 publisher
+        publisher_ = this->create_publisher<std_msgs::msg::String>(
+            "chatter", 10);
+
         timer_ = this->create_wall_timer(
             std::chrono::seconds(1),
             std::bind(&Talker::timer_callback, this));
     }
 
 private:
+
     void timer_callback()
     {
-        RCLCPP_INFO(this->get_logger(), "Hello ROS2 Docker");
+        auto msg = std_msgs::msg::String();
+
+        msg.data = "Hello ROS2 Docker";
+
+        publisher_->publish(msg);
+
+        RCLCPP_INFO(this->get_logger(), "Publishing: %s", msg.data.c_str());
     }
+
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
 
     rclcpp::TimerBase::SharedPtr timer_;
 };
 
+
 int main(int argc, char *argv[])
 {
     rclcpp::init(argc, argv);
+
     rclcpp::spin(std::make_shared<Talker>());
+
     rclcpp::shutdown();
+
     return 0;
 }
-
--- start_ros.sh
+++ start_ros.sh
@@ -2,11 +2,20 @@
 # docker-compose -f docker/docker-compose.yml up --build  -d
 # #--force-recreate
 
-# 删除所有旧容器（即使停止或异常）
-docker rm -f $(docker ps -aq)
-# 重新 build 并启动容器
-docker-compose -f docker/docker-compose.yml up --build --force-recreate -d
+# # 删除所有旧容器（即使停止或异常）
+# docker rm -f $(docker ps -aq)
+# # 重新 build 并启动容器
+# docker compose -f docker/docker-compose.yml up --build --force-recreate -d
 
-# 删除所有旧镜像（即使被容器使用）
-docker images -aq  # 列出所有镜像 ID
-docker rmi -f $(docker images -aq)
+# # 删除所有旧镜像（即使被容器使用）
+# docker images -aq  # 列出所有镜像 ID
+# docker rmi -f $(docker images -aq)
+
+# 在docker目录下
+cd /home/lsj/mine_project/docker
+# 停止并删除旧容器
+docker compose down
+# 强制重新构建镜像（关键）
+docker compose build --no-cache
+# 启动容器
+docker compose up
